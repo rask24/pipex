ebd4dce716ae796bd7e467f313ca780d4eb8480a
rask24 <70057885+rask24@users.noreply.github.com>
2024-02-25 23:27:39 +0900

    Revert "feature/cat only"

--- include/pipex.h
+++ include/pipex.h
@@ -6,18 +6,16 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:36:23 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/25 19:56:54 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:03:43 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "libft.h"
+# include "../libft/libft.h"
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-
-# define CHILD 0
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,32 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:36:05 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/25 22:43:07 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:03:38 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include "utils.h"
-#include "wrapper.h"
-#include "process.h"
 
-static void	_check_argument(int argc)
+int	main(void)
 {
-	if (argc != 5)
-		exit_with_message(__func__, "The number of arguments must be 4");
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	int		fds[2];
-	pid_t	pid;
-
-	_check_argument(argc);
-	xpipe(fds);
-	pid = xfork();
-	if (pid == CHILD)
-		execute_child_process(argv[1], argv[2], fds, envp);
-	else
-		execute_parent_process(argv[4], argv[3], fds, envp);
+	ft_printf("This is main file\n");
 	return (0);
 }
