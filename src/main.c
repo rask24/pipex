27f07063122aae2d20c827374d2fe2722fff4695
rask24 <rask0x18@gmail.com>
2024-02-25 19:02:17 +0900

    pending

--- src/main.c
+++ src/main.c
@@ -6,14 +6,29 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:36:05 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/21 15:03:38 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/25 19:01:48 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **env)
 {
-	ft_printf("This is main file\n");
+	char	*cmd[3];
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		ft_printf("I am Child Process: %d\n", pid);
+		cmd[0] = ft_strdup("ls");
+		cmd[1] = ft_strdup("-l");
+		cmd[2] = NULL;
+		execve("/bin/ls", cmd, env);
+		exit(0);
+	}
+	ft_printf("I am Parent Process: %d\n", pid);
+	(void)argc;
+	(void)argv;
 	return (0);
 }
