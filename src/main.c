233759e6cbb3e80b5580c165370dd6118295eec4
rask24 <70057885+rask24@users.noreply.github.com>
2024-02-27 13:02:56 +0900

    feature relative path (#9)
    * implement execute_direct_path
    
    * fix process with waitpid
    
    * add rspec test
    
    * refactor rspec test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:36:05 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/25 22:43:07 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/27 01:18:39 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	main(int argc, char **argv, char **envp)
 	pid = xfork();
 	if (pid == CHILD)
 		execute_child_process(argv[1], argv[2], fds, envp);
-	else
-		execute_parent_process(argv[4], argv[3], fds, envp);
+	waitpid(CHILD, NULL, 0);
+	execute_parent_process(argv[4], argv[3], fds, envp);
 	return (0);
 }
--- src/process/execute_command.c
+++ src/process/execute_command.c
@@ -6,17 +6,33 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:29:05 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/26 16:12:09 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/26 22:14:05 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "process.h"
+#include "utils.h"
 
-static void	not_found_exit(const char *cmd_name)
+static void	_not_found_exit(const char *cmd_name)
 {
 	ft_dprintf(STDERR_FILENO, "pipex: %s: command not found\n", cmd_name);
 	exit(NOT_FOUND);
+}
+
+static void	_permission_denied_exit(const char *cmd_name)
+{
+	ft_dprintf(STDERR_FILENO, "pipex: %s: Permission denied\n", cmd_name);
+	exit(PERMISSION_DENIED);
+}
+
+static void	_execute_direct_path(char **cmd_list, char **envp)
+{
+	if (access(cmd_list[0], X_OK) == SUCCESS)
+		execve(cmd_list[0], cmd_list, envp);
+	if (errno == EACCES)
+		_permission_denied_exit(cmd_list[0]);
+	exit_with_message(cmd_list[0], strerror(errno));
 }
 
 void	execute_command(const char *cmd, char **envp)
@@ -25,8 +41,10 @@ void	execute_command(const char *cmd, char **envp)
 	const char	*exec_path;
 
 	cmd_list = ft_split(cmd, ' ');
+	if (access(cmd_list[0], F_OK) == SUCCESS)
+		_execute_direct_path(cmd_list, envp);
 	exec_path = resolve_command_path(cmd_list[0], envp);
 	if (!exec_path)
-		not_found_exit(cmd_list[0]);
+		_not_found_exit(cmd_list[0]);
 	execve(exec_path, cmd_list, envp);
 }
