ef1bc7b7a4b552bf8d89ddebb7db01e29c49c5e6
rask24 <rask0x18@gmail.com>
2024-03-17 18:30:23 +0900

    implement _execute_heredoc_append_processes mock

--- include/process.h
+++ include/process.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 20:03:54 by reasuke           #+#    #+#             */
-/*   Updated: 2024/03/17 16:22:56 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/03/17 18:26:40 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ pid_t		execute_infile_process(const char *infile_path, const char *cmd,
 				int fds[2], char **envp);
 pid_t		execute_pipe_process(const char *cmd, int prev_fds[2],
 				int next_fds[2], char **envp);
-pid_t		execute_outfile_process(const char *outfile_path, const char *cmd,
-				int fds[2], char **envp);
+pid_t		execute_outfile_overwrite_process(const char *outfile_path,
+				const char *cmd, int fds[2], char **envp);
 void		execute_command(const char *cmd, char **envp);
 const char	*resolve_command_path(const char *cmd_name, char **envp);
 
