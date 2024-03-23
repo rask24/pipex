06eedbe56d1ba439691cdfb96bf7fca61b6cd2e0
rask24 <rask0x18@gmail.com>
2024-03-23 19:38:49 +0900

    implement xclose

--- src/wrapper/xclose.c
+++ src/wrapper/xclose.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wrapper.h                                          :+:      :+:    :+:   */
+/*   xclose.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/25 19:16:55 by reasuke           #+#    #+#             */
-/*   Updated: 2024/03/23 19:38:01 by reasuke          ###   ########.fr       */
+/*   Created: 2024/03/23 19:33:51 by reasuke           #+#    #+#             */
+/*   Updated: 2024/03/23 19:37:45 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WRAPPER_H
-# define WRAPPER_H
+#include "wrapper.h"
 
-# include "constants.h"
-# include "utils.h"
-# include <errno.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+int	xclose(int fd)
+{
+	int	ret;
 
-char	**ft_xsplit(const char *s, char c);
-char	*ft_xstrjoin(const char *s1, const char *s2);
-int		xclose(int fd);
-int		xdup2(int fd1, int fd2);
-int		xfork(void);
-int		xpipe(int fds[2]);
-
-#endif
+	ret = close(fd);
+	if (ret == -1)
+		exit_with_message(__func__, strerror(errno), FUNCTION_FAIL);
+	return (ret);
+}
