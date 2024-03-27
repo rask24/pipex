e737957a15f6f5c395b9f88259542afe1c3ea347
rask24 <70057885+rask24@users.noreply.github.com>
2024-03-27 15:03:59 +0900

    Fix/handle escape (#34)
    * add unit test
    
    * add rspec test
    
    * fix handle escape
    
    * fix norm
    
    ---------
    
    Co-authored-by: reasuke <reasuke@student.42tokyo.jp>

--- src/split_cmd/split_cmd.c
+++ src/split_cmd/split_cmd.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 17:13:41 by reasuke           #+#    #+#             */
-/*   Updated: 2024/03/15 18:59:44 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/03/27 15:01:19 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,17 @@ static char	**_generate_commands(t_list *token_list)
 	return (ret);
 }
 
-static char	*_process_escape(char *str)
+static int	_shold_escape(char c, bool is_dq)
+{
+	if (is_dq)
+		return (c == '"' || c == '\\');
+	else
+		return (c == '(' || c == ')' || c == '<' || c == '>' || c == '`'
+			|| c == '!' || c == '#' || c == '&' || c == '|' || c == ';'
+			|| c == '.' || c == ':' || c == '\'' || c == '$' || c == '\\');
+}
+
+static char	*_process_escape(char *str, bool is_dq)
 {
 	int	len;
 	int	i;
@@ -41,7 +51,7 @@ static char	*_process_escape(char *str)
 	i = 0;
 	while (i < len)
 	{
-		if (str[i] == '\\' && (str[i + 1] == '"' || str[i + 1] == '\\'))
+		if (str[i] == '\\' && _shold_escape(str[i + 1], is_dq))
 		{
 			ft_memmove(&str[i], &str[i + 1], len - i);
 			len--;
@@ -53,25 +63,25 @@ static char	*_process_escape(char *str)
 
 static void	_process_token(t_list *token_list)
 {
-	const char	*tmp;
+	char		*tmp;
 	char		*trimed;
 
 	while (token_list)
 	{
+		tmp = (char *)get_token(token_list)->content;
 		if (get_token(token_list)->type == TK_SINGLE_QUOTE)
 		{
-			tmp = get_token(token_list)->content;
-			get_token(token_list)->content = ft_strtrim(
-				get_token(token_list)->content, "\'");
-			free((char *)tmp);
+			get_token(token_list)->content = ft_strtrim(tmp, "\'");
+			free(tmp);
 		}
-		if (get_token(token_list)->type == TK_DOUBLE_QUOTE)
+		else if (get_token(token_list)->type == TK_DOUBLE_QUOTE)
 		{
-			tmp = get_token(token_list)->content;
-			trimed = ft_strtrim(get_token(token_list)->content, "\"");
-			get_token(token_list)->content = _process_escape(trimed);
-			free((char *)tmp);
+			trimed = ft_strtrim(tmp, "\"");
+			get_token(token_list)->content = _process_escape(trimed, true);
+			free(tmp);
 		}
+		else
+			get_token(token_list)->content = _process_escape(tmp, false);
 		token_list = token_list->next;
 	}
 }
