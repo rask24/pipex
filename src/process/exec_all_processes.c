abeb9b834df72b734f97b1f64c3efe9ae2bc679b
rask24 <rask0x18@gmail.com>
2024-05-29 20:15:54 +0900

    delete unnecessary function

--- src/process/exec_all_processes.c
+++ src/process/exec_all_processes.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 12:38:05 by reasuke           #+#    #+#             */
-/*   Updated: 2024/05/29 17:28:56 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/05/29 20:15:44 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,6 @@ static void	_close_pipe_end(int fds[2])
 {
 	xclose(fds[0]);
 	xclose(fds[1]);
-}
-
-// TODO: delete
-__attribute__((unused)) static void	_dev_display_pr(t_process *pr)
-{
-	ft_dprintf(STDERR_FILENO, "mode: %d\n", pr->mode);
-	ft_dprintf(STDERR_FILENO, "file_path: %s\n", pr->file_path);
-	ft_dprintf(STDERR_FILENO, "cmd: %s\n", pr->cmd);
-	ft_dprintf(STDERR_FILENO, "pipe_fd: %d, %d\n", pr->pipe_fd[0],
-		pr->pipe_fd[1]);
-	if (pr->next_pipe_fd)
-		ft_dprintf(STDERR_FILENO, "next_pipe_fd: %d, %d\n", pr->next_pipe_fd[0],
-			pr->next_pipe_fd[1]);
-	ft_dprintf(STDERR_FILENO, "delimiter: %s\n", pr->delimiter);
 }
 
 pid_t	*exec_all_processes(t_ctx *ctx, char **envp)
