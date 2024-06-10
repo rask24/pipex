708e114e74ec16002608cc1d69410f88e0797d90
rask24 <rask0x18@gmail.com>
2024-06-10 18:35:00 +0900

    fix norm

--- src/wrapper/xexecve.c
+++ src/wrapper/xexecve.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 00:43:39 by reasuke           #+#    #+#             */
-/*   Updated: 2024/06/10 00:47:20 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/10 18:34:34 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include "constants.h"
 #include "utils.h"
 
-void	xexecve(const char *path, char * const *argv, char * const *envp)
+void	xexecve(const char *path, char *const *argv, char *const *envp)
 {
 	if (execve(path, argv, envp) == FAILURE)
 		error_exit(__func__, strerror(errno), FUNCTION_FAIL);
